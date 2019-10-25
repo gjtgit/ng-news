7676d80c3ea520fe7df6b0fa465c2dacd0910d49
Jian Tuan Gao <gjtgaowh@cn.ibm.com>
2019-10-25 22:01:37 +0800

    add Favorite button for headlines.

--- src/app/headlines/headlines.component.ts
+++ src/app/headlines/headlines.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../services/news.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-headlines',
@@ -16,7 +17,7 @@ export class HeadlinesComponent implements OnInit {
   ];
   news : any;
   
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService, private snackBar: MatSnackBar ) { }
 
   ngOnInit() {
     this.getCategoryData(this.categories[0]);
@@ -34,5 +35,19 @@ export class HeadlinesComponent implements OnInit {
         this.news = data;
       }
     );
+  }
+
+  onFavrite(article){
+    console.log(article);
+    let items = [];
+    const val = localStorage.getItem('items');
+    if(val != null){
+      items = JSON.parse(val);
+    }
+    items.push(article);
+    localStorage.setItem('items',JSON.stringify(items));
+    this.snackBar.open('Favorite added','ok',{
+      duration:3000
+    })
   }
 }
